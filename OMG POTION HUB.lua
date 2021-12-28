c8c7655ee44076d1bd27cb1571a341a55d279b92
kidkidkidkidkidkidkid <85466118+kidkidkidkidkidkidkid@users.noreply.github.com>
2021-12-28 03:37:36 -0600

    Update OMG POTION HUB.lua

--- OMG POTION HUB.lua
+++ OMG POTION HUB.lua
@@ -1,3 +1,3 @@
-game.Players.LocalPlayer.Name = "POTIONSTEALAH" -- OMG USERNAME WHITELIST!!!! OMGOMOGMG BEST WHITELIST SYSTEM
+game.Players.LocalPlayer.UserId = 1577054201 -- OMG USERID WHITELIST!!!! OMGOMOGMG BEST WHITELIST SYSTEM
 wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/kidkidkidkidkidkidkid/potionhubleaked/main/POTION%20HUB.lua"))()
